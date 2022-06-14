918181152c3ec57ef56dfe255af5e5f4a9c23c8b
alichinese <lixinyu44@huawei.com>
2022-06-14 22:41:58 +0800

    for temporary

--- scripts/coder.py
+++ scripts/coder.py
@@ -3,7 +3,9 @@
 import argparse
 import yaml
 import os
-from git.repo import Repo
+import git
+import sys
+from concurrent.futures import ThreadPoolExecutor
 
 def analay_yaml(yaml_dir = "conf/manifest.yaml"):
     with open(yaml_dir, 'r') as yaml_file:
@@ -49,20 +51,31 @@ def update_component(base_url,upstream,items):
     t = 50
     total = len(items)
     per = t / total
+    thread_pool = ThreadPoolExecutor(max_workers = 15)
     for index,item in enumerate(items):
-        process = (index+1)/total * 100
-        finish = "#" * int((index + 1) * per)
-        need = "-" * int((total - index - 1) * per)
-        item_name = "{0:<30}".format(item["name"])
-        print("\r {:^3.0f}%[{}{}] now cloneing...: {}".format(process, finish, need, item_name), end = "")
-        local_path = os.path.join(parent_dir, item['name'])
-        if os.path.exists(local_path):
-            repo = Repo(local_path)
-            repo.git.checkout(upstream)
-            repo.git.reset('--hard', item['revision'])
-        else:
-            remote = base_url + item['path'] + ".git"
-            Repo.clone_from(remote, to_path = local_path, branch = upstream)
+        # process = (index+1)/total * 100
+        # finish = "#" * int((index + 1) * per)
+        # need = "-" * int((total - index - 1) * per)
+        # item_name = "{0:<30}".format(item["name"])
+        # print("\r {:^3.0f}%[{}{}] now cloneing...: {}".format(process, finish, need, item_name), end = "")
+        thread_pool.submit(clone_repo,parent_dir=parent_dir,upstream=upstream,base_url=base_url,item=item)
+    thread_pool.shutdown(cancel_futures=True)
+
+def clone_repo(parent_dir, upstream, base_url, item):
+    print("now clone...: %s" % item['name'])
+    local_path = os.path.join(parent_dir, item['name'])
+    if os.path.exists(local_path):
+        repo = git.Repo(local_path)
+        repo.git.checkout(upstream)
+        repo.git.reset('--hard', item['revision'])
+    else:
+        remote = base_url + item['path'] + ".git"
+        git.Repo.clone_from(remote, to_path = local_path, branch = upstream, progress=Progress())
+    print("clone: %s finish" % item['name'])
+
+class Progress(git.remote.RemoteProgress):
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        print('update(%s, %s, %s, %s)' % op_code, cur_count, max_count, message)
 
 def init_args():
     """
@@ -78,7 +91,22 @@ def init_args():
 def usage():
     print("usage")
 
+def unit_test():
+    parent_dir = os.path.join(os.getcwd(),"../..")
+    upstream = "openEuler-22.03-LTS"
+    item = {
+        "name": "libfastjson",
+        "path": "//src-openeuler/libfastjson.git",
+        "revision": "f41e3c0d607ae6c23df288532dce5c8c40aa6e11"
+    }
+    base_url = "https://gitee.com"
+    clone_repo(parent_dir=parent_dir,upstream=upstream,base_url=base_url,item=item)
+
 def main():
+    is_test = True
+    if is_test:
+        unit_test()
+        sys.exit(0)
     args = init_args()
     action=args.action
     if action=="updatesub":
