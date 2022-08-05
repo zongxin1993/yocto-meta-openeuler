9b40fb638f94030cc1cb43a8958ef4220a683a25
fanglinxu <fanglinxu@huawei.com>
2022-08-05 20:01:06 +0800

    yocto: introduce iso support for openeuler
    * refactor image-live.bbclass for openeuler:
      1. mkisofs: use cdrkit instead of cdrtools from openeuler-src
      2. fixup iso Image type for grub: when Image type is zImage, force to use Image
         for this case, we should prepare a Image file in DEPLOY_DIR_IMAGE,
         see: meta-openeuler/recipes-core/images/qemu.inc
      3. arm64 support: use "MACHINE_FEATURES isohybrid" to decouple syslinux with x86 and isohybrid
    
    * add grub recipes and download_code.sh for openeuler, just using tarball from openeuler-src
    
    * add parted recipes and download_code.sh for openeuler, just using tarball from openeuler-src
    
    * add openeuler-image-live as a live image when build iso
    
    * fit openeuler_hosttools.inc:
      1. add hosttools used by iso and wic, decouple from raspberrypi4
      2. remove libxml2-native from ASSUME_PROVIDED for libxslt-native building bug
    
    * refactor bb images:
      1. fixup boot dir in meta-openeuler/recipes-core/images/qemu.inc: boot dir is need for image install later.
      2. fixup output for copy_openeuler_distro
      3. fixup variable with image.bbclass order:
          IMAGE feature configs such as IMAGE_FSTYPES shuold defined befor openeuler-image-common.inc and core-image
          IMAGE_INSTALL should define after openeuler-image-common.inc
    
    * refactor initramfs-module-install-efi and make live image install.
    
    * make nologin optimize for openeuler-image-live openeuler-image-tiny
    
    Signed-off-by: fanglinxu <fanglinxu@huawei.com>

--- scripts/download_code.sh
+++ scripts/download_code.sh
@@ -234,6 +234,8 @@ download_code()
     update_code_repo src-openeuler/mesa ${SRC_BRANCH}
     update_code_repo src-openeuler/libdrm ${SRC_BRANCH}
     update_code_repo src-openeuler/xorg-x11-proto-devel ${SRC_BRANCH}
+    update_code_repo src-openeuler/grub2 openEuler-22.09
+    update_code_repo src-openeuler/parted openEuler-22.09
 }
 
 # download iSulad related packages
